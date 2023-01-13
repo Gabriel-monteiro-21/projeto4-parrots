c23bfc898498c45cd5153818237e2ef55408e736
Gabriel <bhcmonteiro54@gmail.com>
2023-01-13 15:16:23 -0300

    versão teste

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+prompt("Com quantas cartas você quer jogar?");
+alert("Obrigado por ter digitado");
 function cartaClicavel(clique){
     console.log(clique)
     clique=document.querySelector('.verso').classList.toggle('card');
